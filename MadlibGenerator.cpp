8bdc5925ef9db5745234c4ed40d29a1c5c146d1c
safiakmh <57275721+safiakmh@users.noreply.github.com>
2019-11-01 21:47:47 -0400

    Create MadlibGenerator.cpp
    This

---
+++ MadlibGenerator.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    string holiday;
+    string noun;
+    string place;
+    string person;
+    string adjective; 
+    string bodyPart;
+    string verb;
+    string adjectiveTwo;
+    string nounTwo;
+    string food;
+    string pluralNoun;
+    
+    cout<< "Enter a holiday" <<endl;
+    cin>> holiday
+    
+    cout<< "Enter a noun<< endl"
+    cin>> noun;
+
+    cout<<"Enter a place" <<endl;
+    cin>> place;
+
+    cout<< "Enter a person" <<endl;
+    cin>> person
+
+    cout<< "Enter an adjective" <<endl;
+    cin>> adjective;
+
+    cout<< "Enter a bodyPart" <<endl;
+    cin>> bodyPart;
+
+    cout<< "Enter a verb" <<endl;
+    cin>> verb;
+
+    cout<< "Enter another adjective" <<endl;
+    cin>> adjectiveTwo
+
+    cout<< "Enter another noun" <<endl;
+    cin>> nounTwo
+
+    cout<< "Enter a food item" <<endl;
+    cin>> food
+
+    cout<< "Enter a plural noun" <<endl;
+    cin>> pluralNoun
+    
+    cout << "I can't believe it's already " <<holiday<<
+    "I can't wait to put on my " <<noun<<
+    "and visit every " <<place<< 
+    "in my neighborhood. This year, I am going to dress up as (a) " <<person<<
+    "with "<<adjective <<bodyPart<< 
+    ". Before I " <<verb<< ", I make sure to grab my " <<adjectiveTwo<< nounTwo<<
+    "to hold all of my "<<food<< ". Finally, all of my " <<pluralNoun<<
+    "are ready to go!"
+
+    return 0;
+}
